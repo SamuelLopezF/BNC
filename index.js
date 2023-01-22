48acd2e73b28e3b40ce32f9bb8e8b4be64e4885e
m0sys <21181457+m0sys@users.noreply.github.com>
2023-01-22 01:53:36 -0500

    feat: hacky animation works!!

--- index.js
+++ index.js
@@ -39,16 +39,6 @@ xhr.onload = function() {
     data_set[1] = value_set
 
 
-    // for(let i = 0 ; i < 10; i ++)
-    // {
-    //   single_message = data_response[i]
-    //   label = single_message.OrderID
-    //   data_set = updateDataSet(data_set, label)    
-    //   updateChart(chart, data_set)
-    //   // sleep(1000)
-    // }
-
-
   } else {
     console.log("could not load data")
   }
@@ -62,18 +52,56 @@ data_set[0] = key_set
 data_set[1] = value_set
 
 xhr.send();
+const delay = (delayInms) => {
+  return new Promise(resolve => setTimeout(resolve, delayInms));
+}
+
+const sample = async (counter) => {
+  console.log('a');
+  console.log('waiting...')
+  stupid_function(counter++);
+  let delayres = await delay(1000);
+  console.log('b');
+}
 
 $.getJSON("/Hackathon/AequitasData.json", function (data) {
   console.log(data);
   console.log("MAFANCULO")
   let counter = 0
+
+    // while (true) {
+    //     stupid_function(counter++);
+    // }
+  //sample(counter);
+
+  // setTimeout(async () => {
+  //    await stupid_function(counter++);
+
+  //  }, 1000);
+
+function myLoop() {         //  create a loop function
+  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
+      stupid_function(counter++)
+    i++;                    //  increment the counter
+    if (i < data_response.length) {           //  if the counter < 10, call the loop function
+      myLoop();             //  ..  again which will trigger another 
+    }                       //  ..  setTimeout()
+  }, 100)
+}
+
+myLoop();
   document.addEventListener("click", function()
   {
     stupid_function(counter)
     counter++
   })
 
-})
+});
+
+var i = 1;                  //  set your counter to 1
+
+
+
 
 function stupid_function(counter)
 {
@@ -86,8 +114,11 @@ function stupid_function(counter)
 
 
 
+/*
+ * Check if dataset contains label and increment request stage as required.
+ */
 function updateDataSet(data_set, label){
-  if(data_set[0] == undefined || data_set[0].length == 0 )
+  if (data_set[0] == undefined || data_set[0].length == 0)
   {
     data_set[0].push(label)
     data_set[1].push(1)
@@ -119,22 +150,12 @@ function updateChart(chart,data_set)
   chart.update();
 }
 
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-// function updateChartData(chart, label, data )
-// { 
-//   console.log("label from the add function : \t" + label)
-//   chart.data.labels.push(label);
-//   chart.data.datasets.forEach((dataset) => {
-//       dataset.data.push(data);
-//   });
-//   chart.update();
+// function sleep(milliseconds) {
+//   const date = Date.now();
+//   let currentDate = null;
+//   do {
+//     currentDate = Date.now();
+//   } while (currentDate - date < milliseconds);
 // }
 
 
@@ -156,117 +177,4 @@ async function createDelay(delay)
 {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
-
-// const parsed_aeq = JSON.parse(aeq_raw_data)
-
-// console.log(parsed_aeq[1])
-
-// console.log(" AEQ  L : " + aeq_raw_data_length)
-// console.log(" ALPHA  L : " + alpha_raw_data_length)
-// console.log(" TSX  L : " + tsx_raw_data_length)
-
-// let aeq_map = new Map()
-
-// let start_epoch = 1673015280013814157
-// let end_epoch = 1673015519989216489
-
-// // await new Promise(r => setTimeout(r, 10));
-// // let increment_of_epoch = 1000
-
-
-  
-  
-// function addData(chart, label, data) {
-//   chart.data.labels.push(label);
-//   chart.data.datasets.forEach((dataset) => {
-//       dataset.data.push(data);
-//   });
-//   chart.update();
-// }
-
-// function removeData(chart) {
-//   chart.data.labels.pop();
-//   chart.data.datasets.forEach((dataset) => {
-//       dataset.data.pop();
-//   });
-//   chart.update();
-// }
-
-
-
-// let counter_unique = 0
-
-// for (let i = 0 ; i < parsed_aeq.length ; i ++)
-// {
-//   let key = parsed_aeq[i].OrderID
-//   if(aeq_map.has(key))
-//   {
-//     let value = aeq_map.get(key)
-//     value++
-//     aeq_map.set(key, value)
-//     //  updateChart()
-//   }else{
-    
-//     counter_unique++
-//     aeq_map.set(key, 1)
-//     //  updateChart()
-//   }
-
-//   // sleep for 10 ms
-
-// }
-
-// const iteration_over_aeq_map = aeq_map.entries();
-
-// for(let i = 0 ; i < aeq_map.size ; i ++)
-// {
-//   // console.log(iteration_over_aeq_map.next().value)
-
-//   let temp = iteration_over_aeq_map.next().value
-//   if(temp[1] == 2  || temp[1] == 4 )
-//   {
-//     // console.log(temp[0])      
-//     // console.log("TRANSACTION APPROVED")
-//   }else{
-//     console.log(temp[0])
-//     console.log(temp[1])
-//     console.log("\t\tANOMALY DETECTED" )  
-//   }
-
-// }
-// async function sleep(ms) {
-//  
-// }
-// const ctx = document.getElementById('myChart');
-
-// new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       borderWidth: 1
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   }
-// });
-
-
-
-// //  if any bar = 4 remove it
-
-// // update chart remove old bars
-
-// // update dataset for chart
-
-// // render chart
-
-// // wait 10 ms
 
