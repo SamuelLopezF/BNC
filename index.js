dfc91d05fce3b625d9c4d485dfb82d2233a8d296
TheBarzani <ismaelmergasori@gmail.com>
2023-01-22 06:39:58 -0500

    removed Samuel's comments

--- index.js
+++ index.js
@@ -103,8 +103,6 @@ function updateDataSet(data_set, label, message_direction){
     for(let i = 0 ; i < data_set[0].length; i ++)
     { 
 
-      //data_set[1][i] = STATE REPRESENTATION integer 1->4
-      //data_Set[2][i] = COLOR REPRESENTATION
       if(data_set[0][i] === label)
       {
         if(data_set[1][i] == 1 && message_direction === "ExchangeToNBF")
@@ -157,15 +155,6 @@ function sleep(milliseconds) {
   } while (currentDate - date < milliseconds);
 }
 
-// function updateChartData(chart, label, data )
-// { 
-//   console.log("label from the add function : \t" + label)
-//   chart.data.labels.push(label);
-//   chart.data.datasets.forEach((dataset) => {
-//       dataset.data.push(data);
-//   });
-//   chart.update();
-// }
 
 
 
@@ -187,116 +176,4 @@ async function createDelay(delay)
   return new Promise(resolve => setTimeout(resolve, delay));
 }
 
-// const parsed_aeq = JSON.parse(aeq_raw_data)
-
-// console.log(parsed_aeq[1])
-
-// console.log(" AEQ  L : " + aeq_raw_data_length)
-// console.log(" ALPHA  L : " + alpha_raw_data_length)
-// console.log(" TSX  L : " + tsx_raw_data_length)
-
-// let aeq_map = new Map()
-
-// let start_epoch = 1673015280013814157
-// let end_epoch = 1673015519989216489
-
-// // await new Promise(r => setTimeout(r, 10));
-// // let increment_of_epoch = 1000
-
-
-  
-  
-// function addData(chart, label, data) {
-//   chart.data.labels.push(label);
-//   chart.data.datasets.forEach((dataset) => {
-//       dataset.data.push(data);
-//   });
-//   chart.update();
-// }
-
-// function removeData(chart) {
-//   chart.data.labels.pop();
-//   chart.data.datasets.forEach((dataset) => {
-//       dataset.data.pop();
-//   });
-//   chart.update();
-// }
-
-
-
-// let counter_unique = 0
-
-// for (let i = 0 ; i < parsed_aeq.length ; i ++)
-// {
-//   let key = parsed_aeq[i].OrderID
-//   if(aeq_map.has(key))
-//   {
-//     let value = aeq_map.get(key)
-//     value++
-//     aeq_map.set(key, value)
-//     //  updateChart()
-//   }else{
-    
-//     counter_unique++
-//     aeq_map.set(key, 1)
-//     //  updateChart()
-//   }
-
-//   // sleep for 10 ms
-
-// }
-
-// const iteration_over_aeq_map = aeq_map.entries();
-
-// for(let i = 0 ; i < aeq_map.size ; i ++)
-// {
-//   // console.log(iteration_over_aeq_map.next().value)
-
-//   let temp = iteration_over_aeq_map.next().value
-//   if(temp[1] == 2  || temp[1] == 4 )
-//   {
-//     // console.log(temp[0])      
-//     // console.log("TRANSACTION APPROVED")
-//   }else{
-//     console.log(temp[0])
-//     console.log(temp[1])
-//     console.log("\t\tANOMALY DETECTED" )  
-//   }
-
-// }
-// async function sleep(ms) {
-//  
-// }
-// const ctx = document.getElementById('myChart');
-
-// new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       borderWidth: 1
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   }
-// });
-
-
-
-// //  if any bar = 4 remove it
-
-// // update chart remove old bars
-
-// // update dataset for chart
-
-// // render chart
-
-// // wait 10 ms
 
